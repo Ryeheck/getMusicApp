d4bfa3feb6a79374fe0f95b36a294565a40b1796
rynickinroman <ryanickinroman@gmail.com>
2026-09-10 20:53:55 +0300

    fix: stop tracking build dir

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,4 +0,0 @@
-set(__QT_DEPLOY_TARGET_QuaZip_FILE /home/ryabi/my_apps/qt/getMusicApp/build/_deps/quazip-build/quazip/libquazip1-qt6d.so.1.7.0)
-set(__QT_DEPLOY_TARGET_QuaZip_TYPE SHARED_LIBRARY)
-set(__QT_DEPLOY_TARGET_getMusicApp_FILE /home/ryabi/my_apps/qt/getMusicApp/build/getMusicApp)
-set(__QT_DEPLOY_TARGET_getMusicApp_TYPE EXECUTABLE)
